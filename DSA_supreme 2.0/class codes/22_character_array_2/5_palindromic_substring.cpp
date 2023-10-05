5d42d5f8ded45e382efb18279eafea6eea6abc9b
Onkar Patel <onkarpatel10@gmail.com>
2023-10-05 14:17:24 +0530

    palindromic substring

--- DSA_supreme 2.0/class codes/22_character_array_2/5_palindromic_substring.cpp
+++ DSA_supreme 2.0/class codes/22_character_array_2/5_palindromic_substring.cpp
@@ -1,7 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
+int expand(string s, int start, int end)
+{
+  int count = 0;
+  while (start >= 0 && end < s.length() && s[start] == s[end])
+  {
+    count++;
+    start--;
+    end++;
+  }
+  return count;
+}
+
+int countSubstrings(string s)
+{
+
+  int Totalcount = 0;
+
+  for (int center = 0; center < s.length(); center++)
+  {
+
+    // EVEN
+    int EvenKaCount = expand(s, center, center);
+
+    // ODD
+    int OddKaCount = expand(s, center, center + 1);
+
+    Totalcount = Totalcount + OddKaCount + EvenKaCount;
+  }
+  return Totalcount;
+}
 int main()
 {
-  cout << "hello";
-return 0;
+  string s = "madam";
+  int answer = countSubstrings(s);
+  cout<<answer;
+  return 0;
 }
